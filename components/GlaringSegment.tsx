15d4a498488c35c4d3e52bcd50ffa7f7cc99f355
Takuya Matsuyama <hi@craftz.dog>
2024-12-17 14:00:50 +0900

    feat(glaring-segment): add blooms

--- components/GlaringSegment.tsx
+++ components/GlaringSegment.tsx
@@ -16,9 +16,9 @@ export const GlaringSegment: React.FC<Props> = ({
       <View style={[styles.segment, contentStyle]}>{children}</View>
       <View style={styles.footer}>
         <View style={[styles.bloom, styles.bloomBlue]} />
-        <View style={[styles.bloom]} />
-        <View style={[styles.bloom]} />
-        <View style={[styles.bloom]} />
+        <View style={[styles.bloom, styles.bloomPink]} />
+        <View style={[styles.bloom, styles.bloomOrange]} />
+        <View style={[styles.bloom, styles.bloomPurple]} />
       </View>
     </View>
   );
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
     boxShadow: '0 0 0 1 rgba(255, 255, 255, 0.2)',
   },
   footer: {
-    // overflow: 'hidden',
+    overflow: 'hidden',
     zIndex: 1,
     width: '100%',
     height: 100,
@@ -54,6 +54,36 @@ const styles = StyleSheet.create({
     boxShadow: `
       0 0px 20px 3px hsl(199 89% 48%),
       0 0px 30px 4px hsl(199 89% 48%)
+    `,
+  },
+  bloomPink: {
+    left: `${Math.round(10 + 40 / 3)}%`,
+    width: '40%',
+    marginTop: -16,
+    borderRadius: 10,
+    boxShadow: `
+      0 0px 20px 3px hsl(330 81% 60%),
+      0 0px 30px 4px hsl(330 81% 60%)
+    `,
+  },
+  bloomOrange: {
+    left: `${Math.round(10 + (40 / 3) * 2)}%`,
+    width: '40%',
+    marginTop: -16,
+    borderRadius: 10,
+    boxShadow: `
+      0 0px 20px 3px hsl(25 95% 53%),
+      0 0px 30px 4px hsl(25 95% 53%)
+    `,
+  },
+  bloomPurple: {
+    left: `${10 + 40}%`,
+    width: '40%',
+    marginTop: -16,
+    borderRadius: 10,
+    boxShadow: `
+      0 0px 20px 3px hsl(271 91% 65%),
+      0 0px 30px 4px hsl(271 91% 65%)
     `,
   },
 });
