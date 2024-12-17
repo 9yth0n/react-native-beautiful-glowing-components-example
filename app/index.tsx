5ca485345e6370f2fa4723fbc9a5c6c890875868
Takuya Matsuyama <hi@craftz.dog>
2024-12-17 14:45:44 +0900

    feat(glowing-button): duplicate gradient button

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { GlaringSegment } from '@/components/GlaringSegment';
+import { GlowingButton } from '@/components/GlowingButton';
 import { GradientButton } from '@/components/GradientButton';
 import { StyleSheet, SafeAreaView, Text } from 'react-native';
 
@@ -7,6 +8,7 @@ export default function MainScreen() {
     <SafeAreaView style={styles.container}>
       <GlaringSegment style={styles.segment} >
         <Text style={styles.heading}>Hello</Text>
+        <GlowingButton>Log in</GlowingButton>
         <GradientButton style={styles.buttonSignUp}>Create an account</GradientButton>
       </GlaringSegment>
     </SafeAreaView>
